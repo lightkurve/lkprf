0834b05c16266b49a2ed5710313bf30ac74518d5
Nschanche <nicole.e.schanche@nasa.gov>
2024-09-24 14:56:43 -0400

    Documentation (#10)
    * Moved _update_coordinates
    
    * Updated Kepler _update_coordinates
    
    * renamed functions
    
    * wording update
    
    * Added tutorial notebook
    
    * Improved tutorial format
    
    * merge with origin main
    
    * Update change file
    
    * fixed formatting

--- src/lkprf/tessprf.py
+++ src/lkprf/tessprf.py
@@ -6,6 +6,7 @@ from .utils import LKPRFWarning
 from .data import get_tess_prf_file
 from . import PACKAGEDIR
 import warnings
+from scipy.interpolate import RectBivariateSpline
 
 from .prfmodel import PRF
 from scipy.interpolate import RectBivariateSpline
@@ -30,7 +31,7 @@ class TESSPRF(PRF):
     def _get_prf_data(self):
         return get_tess_prf_file(camera=self.camera, ccd=self.ccd, sector=self.sector, cache_dir=self.cache_dir)
 
-    def update_coordinates(self, targets: List[Tuple], shape: Tuple):
+    def check_coordinates(self, targets: List[Tuple], shape: Tuple):
         row, column = self._unpack_targets(targets)
         """Check that coordinates are within bounds, raise warnings otherwise."""
         if (np.atleast_1d(column) < 45).any():
@@ -39,22 +40,58 @@ class TESSPRF(PRF):
                 LKPRFWarning,
             )
 
-        if ((np.atleast_1d(column) + shape[1]) > 2092).any():
+        if ((np.atleast_1d(column) + shape[1]) >= 2093).any():
+
             warnings.warn(
-                "`targets` contains collateral pixels: Column(s) > 2093 ",
+                "`targets` contains collateral pixels: Column(s) >= 2093 ",
                 LKPRFWarning,
             )
-        if (np.atleast_1d(row) < 0).any():
-            warnings.warn(
-                "`targets` contains collateral pixels: Row(s) < 0",
-                LKPRFWarning,
-            )
+
         if ((np.atleast_1d(row) + shape[0]) > 2048).any():
             warnings.warn(
                 "`targets` contains collateral pixels: Row(s) > 2048)",
                 LKPRFWarning,
             )
-        self._update_coordinates(targets=targets, shape=shape)
+
+        return
+    
+
+    def _prepare_supersamp_prf(self, targets: List[Tuple], shape: Tuple):
+        row, column = self._unpack_targets(targets)
+        # Set the row and column for the model. 
+
+        # Create one supersampled PRF for each image, used for all targets in the image
+        row, column = np.atleast_1d(row), np.atleast_1d(column)
+        row = np.min([np.max([row, row**0 * 0], axis=0), row**0 * 2048], axis=0).mean()
+        column = np.min(
+            [np.max([column, column**0 * 45], axis=0), column**0 * 2092], axis=0
+        ).mean()
+
+        # interpolate the calibrated PRF shape to the target position
+        min_prf_weight = 1e-6
+        rowdim, coldim = shape[0], shape[1]
+        ref_column = column + 0.5 * coldim
+        ref_row = row + 0.5 * rowdim
+        supersamp_prf = np.zeros(self.PRFdata.shape[1:], dtype="float32")
+
+        # Find the 4 measured PRF models nearest the target location
+        prf_weights = [
+            np.sqrt(
+                (ref_column - self.crval1p[i]) ** 2 + (ref_row - self.crval2p[i]) ** 2) 
+                for i in range(self.PRFdata.shape[0])
+                ]
+        idx = np.argpartition(prf_weights, 4)[:4]
+
+        for i in idx:
+            if prf_weights[i] < min_prf_weight:
+                prf_weights[i] = min_prf_weight
+            supersamp_prf += self.PRFdata[i] / prf_weights[i]
+
+
+        supersamp_prf /= np.nansum(supersamp_prf) * self.cdelt1p[0] * self.cdelt2p[0]
+        
+        # Set up the interpolation function
+        self.interpolate = RectBivariateSpline(self.PRFrow, self.PRFcol, supersamp_prf)
         return
     
 
